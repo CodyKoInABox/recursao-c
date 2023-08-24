c71e913c6e8bb40482a1aaeb6361d972ddc58d0c
Cody Ko In A Box <codykoinabox@gmail.com>
2023-08-24 20:52:32 -0300

    Create exercicio2.c

---
+++ exercicio2.c
@@ -0,0 +1,1 @@
+/// Usando conceito de recursividade construa um algoritmo, onde possuem uma funcao que informar o valor de um termo da serie de Fibonacci. Obs. Sabendo que a serie e 1, 1, 2, 3, 5, 8, 13, 21, 34
