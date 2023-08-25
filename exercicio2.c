88e1383219334724e627220bc62066b70a3950b7
CodyKoInABox <codykoinabox@gmail.com>
2023-08-25 10:14:36 -0300

    Exercicio 2

--- exercicio2.c
+++ exercicio2.c
@@ -1,7 +1,7 @@
 // Usando conceito de recursividade construa um algoritmo, onde possuem uma funcao que informar o valor de um termo da serie de Fibonacci. Obs. Sabendo que a serie e 1, 1, 2, 3, 5, 8, 13, 21, 34
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int fibonacci(int pos){
     if(pos == 0 || pos == 1){
@@ -12,8 +12,16 @@ int fibonacci(int pos){
 }
 
 int main(){
+
+    int pos;
+
+    printf("Qual numero da sequencia de Fibonacci voce deseja calcular? ");
+    scanf("%d", &pos);
     
-    printf("%d", fibonacci(5));
+    printf("%d", fibonacci(pos));
+
+    printf("\n");
+    system("pause");
 
     return 0;
 }
